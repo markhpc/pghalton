96a648cbde6c3c5cb40f6f22102e56048ccdf33a
Mark Nelson <mnelson@redhat.com>
2016-02-12 07:46:15 -0600

    Git rid of the acting and secondary distiction.
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- pg.py
+++ pg.py
@@ -12,42 +12,28 @@ class PG:
         potential_osds = gs.get('potential_osds', 0)
         offset = gs.get('offset', 0)
 
-        self.acting_primary = get_bucket(offset+(index*rep), potential_osds)
-        self.acting_secondaries = []
-        for r in xrange(1, rep):
-            self.acting_secondaries.append(get_bucket(offset+(index*rep)+r, potential_osds))
+        self.acting = []
+        for r in xrange(0, rep):
+            self.acting.append(get_bucket(offset+(index*rep)+r, potential_osds))
 
-        self.up_primary = self.acting_primary
-        self.up_secondaries = list(self.acting_secondaries)
+        self.up = list(self.acting)
 
     def using_osd(self, osd):
-       if self.acting_primary == osd:
-          return True
-       if osd in self.acting_secondaries:
+       if osd in self.acting:
           return True
 
     def remap(self, up_map, up_index):
        remap_count = 0
-       # revert to the acting_primary if it's back up
-       if self.up_primary != self.acting_primary:
-           if self.acting_primary in up_map:
-               self.up_primary = self.acting_primary
-               remap_count +=1
-       # If the up_primary is no longer up, find a new one
-       if self.up_primary not in up_map:
-          self.up_primary = up_map[get_bucket(up_index, len(up_map))]
-          up_index+=1
-          remap_count+=1
 
-       for i in xrange(0, len(self.up_secondaries)):
-          # rever to the acting_secondary if it's back up
-          if self.up_secondaries[i] != self.acting_secondaries[i]:
-              if self.acting_secondaries[i] in up_map:
-                  self.up_secondaries[i] = self.acting_secondaries[i]
+       for i in xrange(0, len(self.up)):
+          # rever to the acting OSD if it's back up
+          if self.up[i] != self.acting[i]:
+              if self.acting[i] in up_map:
+                  self.up[i] = self.acting[i]
                   remap_count+=1
-          # If the up_secondary is no longer up, find a new one
-          if self.up_secondaries[i] not in up_map:
-              self.up_secondaries[i] = up_map[get_bucket(up_index, len(up_map))]
+          # If the up OSD is no longer up, find a new one
+          if self.up[i] not in up_map:
+              self.up[i] = up_map[get_bucket(up_index, len(up_map))]
               up_index+=1
               remap_count+=1
        PG.REMAP_COUNT+=remap_count
@@ -65,16 +51,8 @@ class PG:
        print "pg: %s, acting: %s, up: %s" % (self.index, (self.acting_primary, self.acting_secondaries), (self.up_primary, self.up_secondaries))
 
     def get_acting(self):
-       ret = []
-       ret.append(self.acting_primary)
-       for osd in self.acting_secondaries:
-           ret.append(osd)
-       return ret
+        return self.acting
 
     def get_up(self):
-       ret = []
-       ret.append(self.up_primary)
-       for osd in self.up_secondaries:
-           ret.append(osd)
-       return ret
+        return self.up
 
